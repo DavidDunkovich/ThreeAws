442ecef33f061069000fd0cdedcdc282825c25b8
David Dunkovich <dunkovichd@gmail.com>
2020-08-21 15:59:01 -0700

    3D spinning AWS logo

---
+++ ThreeAws.js
@@ -0,0 +1,14 @@
+
+import React, { Component } from "react";
+import './App.css';
+import Aws from './3d/Aws';
+
+class App extends Component {
+  render() {
+    return (
+      <Aws />
+    )
+  }
+}
+
+export default App;
